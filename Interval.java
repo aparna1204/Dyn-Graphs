15b37acf0920dbf32f04b3ddb019435c8397441f
Aparna Shankar <aparna.shankar.12@gmail.com>
2017-06-28 20:38:09 +0530

    Add javadoc

--- Interval.java
+++ Interval.java
@@ -1,25 +1,34 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 /**
- *
+ * a class to store one or two vertex sets
+ * 
  * @author Aparna Shankar
  */
-// this is kind of a weird class to store 1 or 2 vertex sets
 public class Interval {
     int startpt1;
     int endpt1;
     int startpt2;
     int endpt2;
 
+    /**
+     * constructor for only one vertex set
+     * 
+     * @param startpt the start point of the interval
+     * @param endpt the end point of the interval
+     */
     public Interval(int startpt, int endpt) {
         this.startpt1 = startpt;
         this.endpt2 = endpt;
     }
 
+    /**
+     * constructor for two vertex sets
+     *
+     * @param startpt1 start point of the first interval
+     * @param endpt1 end point of the first interval
+     * @param startpt2 start point of the second interval
+     * @param endpt2 end point of the second interval
+     */
     public Interval(int startpt1, int endpt1, int startpt2, int endpt2) {
         this.startpt1 = startpt1;
         this.endpt1 = endpt1;
@@ -27,6 +36,11 @@ public class Interval {
         this.endpt2 = endpt2;
     }
     
+    /**
+     * constructor from a SparseTreeNode
+     * 
+     * @param stn the SparseTreeNode, will match start/endpts with vertex sets stn is made of
+     */
     public Interval(SparseTreeNode stn){
         startpt1 = stn.vn1[0];
         endpt1 = stn.vn1[stn.vn1.length-1];
@@ -34,10 +48,13 @@ public class Interval {
         endpt2 = stn.vn2[stn.vn2.length-1];
     }
     
-    public boolean contains(int i){
-        return (i>=startpt1 && i<=endpt1);
-    }
-    
+    /**
+     * checks if a given edge has endpoints in the given vertex sets
+     * 
+     * @param i endpoint of the edge
+     * @param j endpoint of the edge
+     * @return true if each endpoint is in one vertex set, false otherwise
+     */
     public boolean contains(int i, int j){// i and j are endpoints of an edge
         if ((i>=startpt1 && i<=endpt1)&&(j>=startpt2 && j<=endpt2)){
             return true;
